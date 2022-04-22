de65d82977d7c3b4cef6d8d678deea57c05767f3
Parth Sareen <parthsareen2006@gmail.com>
2022-04-22 20:35:49 +0530

    Pushed to github

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var winHeight = $(window).innerHeight();
+$(document).ready(function () {
+  $(".Basic_setup").height(winHeight);
+  $("body").height(winHeight * $(".Basic_setup").length);
+});
+window.addEventListener("resize", function (event) {
+  $(".Basic_setup").height($(window).innerHeight());
+});
+$(window).on("scroll", function () {
+  $(".Space").css("bottom", $(window).scrollTop() * -1);
+});
