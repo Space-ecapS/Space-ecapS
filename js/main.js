bf7b4930a1c8c1a320bbbae31b7084ebca0fb87f
Parth Sareen <parthsareen2006@gmail.com>
2022-04-24 21:46:54 +0530

    Added text animation
    It works in Ecpas portion but still need to work on Space side.

--- js/main.js
+++ js/main.js
@@ -17,4 +17,13 @@ ScrollOut({
 
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to('#Ecaps_text_anime_1', {ScrollTrigger : "#Ecaps_text_anime_1", opacity: 1, duration: 3, x: 0,});
+gsap.to(".Ecaps_text_anime_1", { scrollTrigger: { trigger: '.Ecaps_text_anime_1', toggleActions: "restart none none none" }, x: "0px", opacity: 1,duration: 1 });
+gsap.to(".Ecaps_text_anime_2", { scrollTrigger: { trigger: '.Ecaps_text_anime_2', toggleActions: "restart none none none" }, x: "0px", opacity: 1, duration: 1 });
+gsap.to(".Ecaps_text_anime_3", { scrollTrigger: { trigger: '.Ecaps_text_anime_3', toggleActions: "restart none none none" }, x: "0px", opacity: 1, duration: 1 });
+gsap.to(".Ecaps_text_anime_4", { scrollTrigger: { trigger: '.Ecaps_text_anime_4', toggleActions: "restart none none none" }, x: "0px", opacity: 1, duration: 1 });
+gsap.to(".Ecaps_text_anime_5", { scrollTrigger: { trigger: '.Ecaps_text_anime_5', toggleActions: "restart none none none" }, x: "0px", opacity: 1, duration: 1 });
+gsap.to(".Ecaps_text_anime_6", { scrollTrigger: { trigger: '.Ecaps_text_anime_6', toggleActions: "restart none none none" }, x: "0px", opacity: 1, duration: 1 });
+gsap.to(".Ecaps_text_anime_7", { scrollTrigger: { trigger: '.Ecaps_text_anime_7', toggleActions: "restart none none none" }, x: "0px", opacity: 1, duration: 1 });
+gsap.to(".Ecaps_text_anime_8", { scrollTrigger: { trigger: '.Ecaps_text_anime_8', toggleActions: "restart none none none" }, x: "0px", opacity: 1, duration: 1 });
+gsap.to(".Ecaps_text_anime_9", { scrollTrigger: { trigger: '.Ecaps_text_anime_9', toggleActions: "restart none none none" }, x: "0px", opacity: 1, duration: 1 });
+gsap.to('.Space_text_anime_1', { scrollTrigger: { trigger: '.Space_text_anime_1', toggleActions: "restart none none none" }, x: "0px", opacity: 1, duration: 1 });
