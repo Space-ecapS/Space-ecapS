203175a1ac3d4cdbf6927a1fd49738b3b732ad3d
Parth Sareen <parthsareen2006@gmail.com>
2022-04-25 21:07:01 +0530

    Gsap and Restructure
    Rejected Gsap for text and restructured the entire html and scss (May add for other stuff)

--- js/main.js
+++ js/main.js
@@ -13,17 +13,3 @@ ScrollOut({
   targets: ".Earth_source",
 });
 
-
-
-gsap.registerPlugin(ScrollTrigger);
-
-gsap.to(".Ecaps_text_anime_1", { scrollTrigger: { trigger: '.Ecaps_text_anime_1', toggleActions: "restart none none none" }, x: "0px", opacity: 1,duration: 1 });
-gsap.to(".Ecaps_text_anime_2", { scrollTrigger: { trigger: '.Ecaps_text_anime_2', toggleActions: "restart none none none" }, x: "0px", opacity: 1, duration: 1 });
-gsap.to(".Ecaps_text_anime_3", { scrollTrigger: { trigger: '.Ecaps_text_anime_3', toggleActions: "restart none none none" }, x: "0px", opacity: 1, duration: 1 });
-gsap.to(".Ecaps_text_anime_4", { scrollTrigger: { trigger: '.Ecaps_text_anime_4', toggleActions: "restart none none none" }, x: "0px", opacity: 1, duration: 1 });
-gsap.to(".Ecaps_text_anime_5", { scrollTrigger: { trigger: '.Ecaps_text_anime_5', toggleActions: "restart none none none" }, x: "0px", opacity: 1, duration: 1 });
-gsap.to(".Ecaps_text_anime_6", { scrollTrigger: { trigger: '.Ecaps_text_anime_6', toggleActions: "restart none none none" }, x: "0px", opacity: 1, duration: 1 });
-gsap.to(".Ecaps_text_anime_7", { scrollTrigger: { trigger: '.Ecaps_text_anime_7', toggleActions: "restart none none none" }, x: "0px", opacity: 1, duration: 1 });
-gsap.to(".Ecaps_text_anime_8", { scrollTrigger: { trigger: '.Ecaps_text_anime_8', toggleActions: "restart none none none" }, x: "0px", opacity: 1, duration: 1 });
-gsap.to(".Ecaps_text_anime_9", { scrollTrigger: { trigger: '.Ecaps_text_anime_9', toggleActions: "restart none none none" }, x: "0px", opacity: 1, duration: 1 });
-gsap.to('.Space_text_anime_1', { scrollTrigger: { trigger: '.Space_text_anime_1', toggleActions: "restart none none none" }, x: "0px", opacity: 1, duration: 1 });
