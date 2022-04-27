be684ddecd7fb3ac2278d12309406ce3e9ccb528
Parth Sareen <parthsareen2006@gmail.com>
2022-04-27 20:21:20 +0530

    Planned to use GSAP with scroll magic instead of scroll trigger
    Scrolltrigger doesn't work with reverse scroll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+gsap.to('.E_anime_1', {duration: .75, opacity: 1, x: 0});
+gsap.to('.E_anime_2', {duration: .75, opacity: 1, x: 0});
+gsap.to('.E_anime_3', {duration: .75, opacity: 1, x: 0});
+gsap.to('.E_anime_4', {duration: .75, opacity: 1, x: 0});
+gsap.to('.E_anime_5', {duration: .75, opacity: 1, x: 0});
+gsap.to('.E_anime_6', {duration: .75, opacity: 1, x: 0});
+gsap.to('.E_anime_7', {duration: .75, opacity: 1, x: 0});
+
+
+
+
 var winHeight = $(window).innerHeight();
 $(document).ready(function () {
   $(".Basic_setup").height(winHeight);
@@ -9,7 +20,7 @@ window.addEventListener("resize", function (event) {
 $(window).on("scroll", function () {
   $(".Space").css("bottom", $(window).scrollTop() * -1);
 });
-ScrollOut({
-  targets: ".Earth_source",
-});
+
+
+
 
