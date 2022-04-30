b2be9c53d4a1dbffca6651967b30ef4f775f6c9a
Parth Sareen <parthsareen2006@gmail.com>
2022-04-30 20:32:25 +0530

    made some changes

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-gsap.to('.E_anime_1', {duration: .75, opacity: 1, x: 0});
-gsap.to('.E_anime_2', {duration: .75, opacity: 1, x: 0});
-gsap.to('.E_anime_3', {duration: .75, opacity: 1, x: 0});
-gsap.to('.E_anime_4', {duration: .75, opacity: 1, x: 0});
-gsap.to('.E_anime_5', {duration: .75, opacity: 1, x: 0});
-gsap.to('.E_anime_6', {duration: .75, opacity: 1, x: 0});
-gsap.to('.E_anime_7', {duration: .75, opacity: 1, x: 0});
-
-
-
-
 var winHeight = $(window).innerHeight();
 $(document).ready(function () {
   $(".Basic_setup").height(winHeight);
@@ -22,5 +11,6 @@ $(window).on("scroll", function () {
 });
 
 
+gsap.regsingePlugin(ScrollTrigger);
 
-
+gsap.to('.Earth_source ')
