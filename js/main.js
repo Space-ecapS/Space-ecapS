46d3a8273320774b02359fc10ad45759c778ad78
Parth Sareen <parthsareen2006@gmail.com>
2022-06-15 02:46:13 +0530

    made real changes

--- js/main.js
+++ js/main.js
@@ -1,34 +1,29 @@
 if (window.screen.width > 600) {
-  var winHeight = $(window).innerHeight();
-  $(document).ready(function () {
-    $(".Basic_setup").height(winHeight);
-    $("body").height(winHeight * $(".Basic_setup").length);
-  });
-  window.addEventListener("resize", function (event) {
-    $(".Basic_setup").height($(window).innerHeight());
-  });
-  $(window).on("scroll", function () {
-    $(".Space").css("bottom", $(window).scrollTop() * -1);
-  });
-
-  var tle = gsap.timeline({ repeat: -1, yoyo: true });
-  var tls = gsap.timeline({ repeat: -1, yoyo: true });
-  tle.fromTo(
-    ".Ecaps_Astronaut",
-    { y: 50 },
-    { duration: 2.5, y: -100, ease: "power1.inOut" }
-  );
-  tls.fromTo(
-    ".Space_Astronaut",
-    { y: 50 },
-    { duration: 2.5, y: -100, ease: "power1.inOut" }
-  );
+    // making the parallel universe effect
+    var winHeight = $(window).innerHeight();
+    $(document).ready(function() {
+        $(".Basic_setup").height(winHeight);
+        $("body").height(winHeight * $(".Basic_setup").length);
+    });
+    window.addEventListener("resize", function(event) {
+        $(".Basic_setup").height($(window).innerHeight());
+    });
+    $(window).on("scroll", function() {
+        $(".Space").css("bottom", $(window).scrollTop() * -1);
+    });
+    // Using gsap to animate the moons
+    var tle = gsap.timeline({ repeat: -1, yoyo: true });
+    var tls = gsap.timeline({ repeat: -1, yoyo: true });
+    tle.fromTo(
+        ".Ecaps_Astronaut", { y: 50 }, { duration: 2.5, y: -100, ease: "power1.inOut" }
+    );
+    tls.fromTo(
+        ".Space_Astronaut", { y: 50 }, { duration: 2.5, y: -100, ease: "power1.inOut" }
+    );
 } else {
-  gsap.registerPlugin(ScrollTrigger);
-  var tle = gsap.timeline({ repeat: -1, yoyo: true });
-  tle.fromTo(
-    ".Ecaps_Astronaut",
-    { y: 100 },
-    { duration: 3, y: -100, ease: "power1.inOut" }
-  );
+    // Using gsap to animate the moon
+    var tle = gsap.timeline({ repeat: -1, yoyo: true });
+    tle.fromTo(
+        ".Ecaps_Astronaut", { y: 100 }, { duration: 3, y: -100, ease: "power1.inOut" }
+    );
 }
